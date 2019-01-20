b43e41499e65e52855ea9995cc94e5076c496699
Yujin Ariza <yujin.ariza@gmail.com>
2019-01-20 14:45:40 -0500

    added build pipeline + lfjs + oflow

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import webcamFlow from './oflow/webcamFlow';
+
+console.log('hello world');
+console.log(webcamFlow);
---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        watchContentBase: true,
+        hot: true,
+        open: true,
+        port: 9000,
+        historyApiFallback: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+});
